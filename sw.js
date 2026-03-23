7e0db6675b12fcd666cab484cdaf038c1d28ce72
joshua041995 <52951178+joshua041995@users.noreply.github.com>
2026-03-23 22:40:57 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,21 @@
 
+const cacheName = 'barcode-pwa-v1';
+const assets = [
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png',
+  './html5-qrcode.min.js'
+];
+
+self.addEventListener('install', evt => {
+  evt.waitUntil(
+    caches.open(cacheName).then(cache => cache.addAll(assets))
+  );
+});
+
+self.addEventListener('fetch', evt => {
+  evt.respondWith(
+    caches.match(evt.request).then(resp => resp || fetch(evt.request))
+  );
+});
